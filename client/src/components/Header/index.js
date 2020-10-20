a2db5ac5c85389a73dde940e39e08e80c41977b8
Eric LaFontsee <elafontsee@gmail.com>
2020-10-20 15:59:43 -0500

    Setup bootstrap cdn

---
+++ client/src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+// import { Link, useLocation } from "react-router-dom";
+// import Navbar from "./Navbar";
+
+function Header() {
+  return (
+    <header className="navbar navbar-expand-lg navbar-light border border-dark">
+      <h1>Eric LaFontsee</h1>
+    </header>
+  );
+}
+
+export default Header;
