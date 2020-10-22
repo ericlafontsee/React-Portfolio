6ff15602183dbd16598af9e4229ae38e47d21422
Eric LaFontsee <elafontsee@gmail.com>
2020-10-22 14:31:00 -0500

    Changed header opacity to 1

--- client/src/components/NavMenu/index.js
+++ client/src/components/NavMenu/index.js
@@ -1,28 +1,28 @@
 import React from "react";
 import "./style.css";
-import { Navbar, Nav } from "react-bootstrap";
+import { Navbar, Nav} from "react-bootstrap";
 
 function NavMenu() {
   return (
-    <Navbar bg="dark">
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav>
-          <Nav.Link  href="/about">
-            About
-          </Nav.Link>
-          <Nav.Link href="/portfolio">
-            Portfolio
-          </Nav.Link>
-          <Nav.Link href="/contact">
-            Contact
-          </Nav.Link>
-          <Nav.Link href="/resume">
-            Resume
-          </Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+      <Navbar className="navbar" collapseOnSelect expand="lg">
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav>
+            <Nav.Link className="text-light" href="/about">
+              About
+            </Nav.Link>
+            <Nav.Link className="text-light" href="/portfolio">
+              Portfolio
+            </Nav.Link>
+            <Nav.Link className="text-light" href="/contact">
+              Contact
+            </Nav.Link>
+            <Nav.Link className="text-light" href="/resume">
+              Resume
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
   );
 }
 
