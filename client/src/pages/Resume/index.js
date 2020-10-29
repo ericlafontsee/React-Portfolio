6b9b17f32f7188c3fb8d3d421c616b7ce3517c3d
Eric LaFontsee <elafontsee@gmail.com>
2020-10-29 07:36:15 -0400

    Added my new resume to the resume page

--- client/src/pages/Resume/index.js
+++ client/src/pages/Resume/index.js
@@ -12,7 +12,7 @@ function Resume() {
     <div className="row mb-3">
         <div className="col-md-12 d-flex justify-content-center">
             <div className=" margin-top ">
-                <a href="https://drive.google.com/file/d/1SeOB1Sipwgn7tsDr4H7k9gwA3twBFAWH/view?usp=sharing">   
+                <a href="https://drive.google.com/file/d/1xXySqfjX-D1bkBxaBjedRBEYPGnbsOKE/view?usp=sharing">   
                 <FontAwesomeIcon icon="download" size="4x" className="icon"/>
                 </a>
                 <img src={process.env.PUBLIC_URL + '/images/resume-2020.png'} className="resume card-img-top img-fluid w-100" alt="Eric LaFontsee's Resume"/>
